3a90a316cebbd064b773661cef9aeff2d21dff4f
Aaron Guzman <guzmanaa@umich.edu>
2022-02-04 21:07:18 -0500

    Login Protocol Implemented
    Client is able to send login request and server will verify and process client login.

--- src/MathMagicServer.java
+++ src/MathMagicServer.java
@@ -4,7 +4,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -16,10 +21,19 @@ public class MathMagicServer {
     
     private static boolean rootLogin = false;
     
+    private static List<String> userList;
+    
+    private static String userLogged;
+      
     public static void main(String[] args) {
         try {
+            
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             
+            userList = Files.readAllLines(Path.of("logins.txt"));
+            
+            userList.remove(" ");
+                       
             System.out.println("Server started at " + new Date());
             
             Socket socket = serverSocket.accept();
@@ -35,7 +49,31 @@ public class MathMagicServer {
                     String command = message.substring(0, message.indexOf(" "));
                     
                     if (command.equalsIgnoreCase("LOGIN")) {
-                        outputToClient.writeUTF("LOGIN COMMAND");
+                        String loginInfo = message.substring(message.indexOf(" ") + 1, message.length());
+                        
+                        if (Pattern.matches(".+ .+", loginInfo)) {
+                            boolean found = false;
+                            for (String user: userList) {
+                                if (user.equals(loginInfo)) {
+                                    userLogged = user;
+                                    
+                                    found = true;
+                                    
+                                    outputToClient.writeUTF("SUCCESS");
+                                    
+                                    break;
+                                }
+                            }
+                            
+                            if (!found) {
+                                outputToClient.writeUTF("FAILURE: Please provide a correct username and password. Try again.");
+                            }
+                        }
+                        else {
+                            outputToClient.writeUTF("301 message format error");
+                        }
+                        
+                        //outputToClient.writeUTF("LOGIN COMMAND");
                     }
                     else if (command.equalsIgnoreCase("SOLVE")) {
                         outputToClient.writeUTF("SOLVE COMMAND");
